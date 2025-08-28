d38d0aceb217c3d99eee67f5e326f43b6f70e0d1
Chase <st10433939@vcconnect.edu.za>
2025-08-28 14:10:15 +0200

    Add project files.

---
+++ 10433939_CLDV6212_POE_P1/Program.cs
@@ -0,0 +1,46 @@
+using _10433939_CLDV6212_POE_P1.Services;
+
+namespace _10433939_CLDV6212_POE_P1.Models
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var configuration = builder.Configuration;
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            //Add services to the container
+            builder.Services.AddControllersWithViews();
+
+            //Register TableStorage with configuration
+            builder.Services.AddSingleton(new TableStorageService(configuration.GetConnectionString("AzureStorage")));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapStaticAssets();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}")
+                .WithStaticAssets();
+
+
+            app.Run();
+        }
+    }
+}
---
+++ 10433939_CLDV6212_POE_P1/Services/TableStorageService.cs
@@ -0,0 +1,48 @@
+﻿using Azure;
+using Azure.Data.Tables;
+using _10433939_CLDV6212_POE_P1.Models;
+
+namespace _10433939_CLDV6212_POE_P1.Services
+{
+    public class TableStorageService
+    {
+        public readonly TableClient _customerTableClient;
+        public TableStorageService(string connectionString)
+        {
+            _customerTableClient = new TableClient(connectionString, "Customer");
+        }
+        public async Task<List<Customer>> GetAllCustomersAsync()
+        {
+            var customers = new List<Customer>();
+
+            await foreach (var customer in _customerTableClient.QueryAsync<Customer>())
+            {
+                customers.Add(customer);
+            }
+            return customers;
+        }
+
+        public async Task AddCustomerAsync(Customer customer)
+        {
+            if (string.IsNullOrEmpty(customer.PartitionKey) || string.IsNullOrEmpty(customer.RowKey))
+            {
+                throw new ArgumentException("PartitionKey and Rowkey must be set.");
+            }
+
+            try
+            {
+                await _customerTableClient.AddEntityAsync(customer);
+            }
+            catch (RequestFailedException ex)
+            {
+                throw new InvalidOperationException("Error adding entity to Table Storage.", ex);
+            }
+        }
+
+        public async Task DeleteCustomerAsync(string partitionKey, string rowKey)
+        {
+            await _customerTableClient.DeleteEntityAsync(partitionKey, rowKey);
+        }
+    }
+}
+
